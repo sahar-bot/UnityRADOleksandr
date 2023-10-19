c74d9126c62b298dcaa8e8fea0a695e1a44b421c
STUDENT Oleksandr Motoryhin <Oleksandr.Motoryhin@students.ittralee.ie>
2023-10-19 10:55:34 +0100

    My first c# script Signed-off-by: STUDENT Oleksandr Motoryhin <Oleksandr.Motoryhin@students.ittralee.ie>

---
+++ MyFirstGame/Assets/CubeControl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class CubeControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Application.targetFrameRate = 60;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += transform.forward;
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) {
+            transform.position += (new Vector3(0, -1, 0));
+                }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            transform.position += (new Vector3(1, 0, 0));
+                }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += (new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+
+            transform.Rotate(Vector3.up, 45);
+        if (Input.GetKeyDown(KeyCode.Q))
+            transform.Rotate(Vector3.up, -45);
+        
+
+        
+
+    }
+}
