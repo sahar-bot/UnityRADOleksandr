a50dcbcae3e3627390a9b3271f17607f65b7693d
STUDENT Oleksandr Motoryhin <Oleksandr.Motoryhin@students.ittralee.ie>
2023-11-16 09:44:10 +0000

    Signed-off-by: STUDENT Oleksandr Motoryhin <Oleksandr.Motoryhin@students.ittralee.ie>

--- MyFirstGame/Assets/CubeControl.cs
+++ MyFirstGame/Assets/CubeControl.cs
@@ -64,7 +64,7 @@ public class CubeControl : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            transform.rotation = Quaternion.Euler(0,transform.rotation.y,0);
+            transform.rotation = Quaternion.Euler(transform.rotation.x,transform.rotation.y,0);
         }
 
 
